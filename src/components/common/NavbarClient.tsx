b6e4083a74dca1749ecea266f9d2ce960d125efe
wilyramos <wily.ramos1@unmsm.edu.pe>
2025-09-07 17:36:09 -0500

    add scroll in navbar

--- src/components/common/NavbarClient.tsx
+++ src/components/common/NavbarClient.tsx
@@ -1,21 +1,37 @@
-// src/components/navbar/NavbarClient.tsx (CLIENT COMPONENT)
+// src/components/navbar/NavbarClient.tsx
 "use client";
 
 import { useEffect, useState } from "react";
 
 export default function NavbarClient({ children }: { children: React.ReactNode }) {
-    const [scrolled, setScrolled] = useState(false);
+    const [show, setShow] = useState(true); // si mostrar el navbar
+    const [lastScrollY, setLastScrollY] = useState(0); // guardamos la última posición de scroll
 
     useEffect(() => {
-        const handleScroll = () => setScrolled(window.scrollY > 10);
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY > lastScrollY && currentScrollY > 50) {
+                // Scroll hacia abajo → ocultar
+                setShow(false);
+            } else {
+                // Scroll hacia arriba → mostrar
+                setShow(true);
+            }
+
+            setLastScrollY(currentScrollY);
+        };
+
         window.addEventListener("scroll", handleScroll);
+
         return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    }, [lastScrollY]);
 
     return (
         <nav
-            className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${scrolled ? "bg-white/90 backdrop-blur-md shadow-sm" : "bg-transparent"
-                }`}
+            className={`fixed top-0 left-0 w-full z-50 transition-transform duration-300  backdrop-blur-md ${
+                show ? "translate-y-0" : "-translate-y-full"
+            }`}
         >
             {children}
         </nav>
